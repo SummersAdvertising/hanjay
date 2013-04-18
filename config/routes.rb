f0efd9f24cc6068a31c5cea0fe82323aaea103ca
Yuzhe <yuzhe.c85@gmail.com>
2013-04-18 11:55:05 +0800

    fix javascript

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Hanjay::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   
   # match ':controller(/:action(/:id))(.:format)'
-  
+
   
   resources :news, :only => [:index, :show]
  
