d47ad303c62624ba49fe89be38b8b9275bcecb3c
Yuzhe <yuzhe.c85@gmail.com>
2013-04-19 20:30:50 +0800

    adjust news scroll

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,6 @@ set :stages, %w(staging production)
 set :default_stage, "staging"
 
 set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
+
+set :keep_releases, 5
+after "deploy:update", "deploy:cleanup" 
